50021eca8c4e0929534bad8d3958b9766762372f
Windfinz <Phongdraco@gmail.com>
2023-11-03 11:52:32 +0700

    scoring

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TileBoard board;
+    public CanvasGroup gameOver;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI hiscoreText;
+
+    private int score;
+
+    private void Start()
     {
-        
+        NewGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewGame()
     {
-        
+        SetScore(0);
+        hiscoreText.text = LoadHiscore().ToString();
+        gameOver.alpha = 0f;
+        gameOver.interactable = false;
+
+        board.ClearBoard();
+        board.CreateTile();
+        board.CreateTile();
+        board.enabled = true;
+
     }
+    
+    public void GameOver()
+    {
+        board.enabled = false;
+        gameOver.interactable = true;
+
+        StartCoroutine(Fade(gameOver, 1f, 1f));
+    }
+
+    private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        float elapsed = 0f;
+        float duration = 0.5f;
+        float from = canvasGroup.alpha;
+
+        while (elapsed < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+
+        }
+        canvasGroup.alpha = to;
+
+    }
+
+    public void InscreaseScore(int points)
+    {
+        SetScore(score + points);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+
+        SaveHiscore();
+    }
+
+    private void SaveHiscore()
+    {
+        int hiscore = LoadHiscore();
+
+        if(score > hiscore)
+        {
+            PlayerPrefs.SetInt("hiscore", score);
+        }
+    }
+
+    private int LoadHiscore()
+    {
+        return PlayerPrefs.GetInt("hiscore", 0);
+    }
+
 }
--- Assets/Script/TileBoard.cs
+++ Assets/Script/TileBoard.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TileBoard : MonoBehaviour
 {
+    public GameManager gameManager;
+
     private TileGrid grid;
 
     private List<Tile> tiles;
@@ -19,14 +21,20 @@ public class TileBoard : MonoBehaviour
 
     }
 
-    private void Start()
+    public void ClearBoard()
     {
-        CreateTile();
-        CreateTile();
-
+        foreach(var cell in grid.cells)
+        {
+            cell.tile = null;
+        }
+        foreach(var tile in tiles)
+        {
+            Destroy(tile.gameObject);
+        }
+        tiles.Clear();
     }
 
-    private void CreateTile()
+    public void CreateTile()
     {
         Tile tile = Instantiate(tilePrefabs, grid.transform);
         tile.SetState(tileStates[0], 2);
@@ -119,6 +127,8 @@ public class TileBoard : MonoBehaviour
         int number = b.number * 2;
 
         b.SetState(tileStates[index], number);
+
+        gameManager.InscreaseScore(number);
     }
 
     private int IndexOf(TileState state)
@@ -155,8 +165,49 @@ public class TileBoard : MonoBehaviour
             CreateTile();
 
         }
-        //TO DO
+
+        if (CheckForGameOver())
+        {
+            gameManager.GameOver();
+        }
 
     }
+
+    private bool CheckForGameOver()
+    {
+        if(tiles.Count != grid.size)
+        {
+            return false;
+        }
+
+        foreach(var tile in tiles)
+        {
+            TileCell up = grid.GetAdjacentCell(tile.cell, Vector2Int.up);
+            TileCell down = grid.GetAdjacentCell(tile.cell, Vector2Int.down);
+            TileCell left = grid.GetAdjacentCell(tile.cell, Vector2Int.left);
+            TileCell right = grid.GetAdjacentCell(tile.cell, Vector2Int.right);
+
+            if(up != null&& CanMerge(tile, up.tile))
+            {
+                return false;
+            }
+            if (down != null && CanMerge(tile, down.tile))
+            {
+                return false;
+            }
+            if (left != null && CanMerge(tile, left.tile))
+            {
+                return false;
+            }
+            if (right != null && CanMerge(tile, right.tile))
+            {
+                return false;
+            }
+
+        }
+            return true;
+
+    }
+
 
 }
